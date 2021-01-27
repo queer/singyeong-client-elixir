fe9b982504df2a51cb7313957ac303e40dbbc9a3
amy null <null@amy.gg>
2021-01-27 09:33:10 -0500

    0.4.0 - selectors, nil apps

--- lib/singyeong/query.ex
+++ lib/singyeong/query.ex
@@ -18,6 +18,11 @@ defmodule Singyeong.Query do
     :"$or",
     :"$nor",
   ]
+  @selector_names [
+    :"$min",
+    :"$max",
+    :"$avg",
+  ]
 
   @type ops() :: [op()] | []
   @type boolean_op_name() ::
@@ -49,19 +54,25 @@ defmodule Singyeong.Query do
     required(binary()) => boolean_op() | logical_op()
   }
 
+  @type selector() ::
+    :"$min"
+    | :"$max"
+    | :"$avg"
+
   typedstruct do
-    field :application, String.t(), enforce: true
+    field :application, String.t() | nil
     field :restricted, boolean(), default: false
     field :key, String.t() | nil, default: nil
     field :droppable, boolean(), default: false
     field :optional, boolean(), default: false
     field :ops, [op()], default: []
+    field :selector, selector() | nil, default: nil
   end
 
   @doc """
   Creates a new query for the given target application
   """
-  @spec new(String.t()) :: __MODULE__.t()
+  @spec new(String.t() | nil) :: __MODULE__.t()
   def new(name), do: %__MODULE__{application: name}
 
   @doc """
@@ -92,5 +103,17 @@ defmodule Singyeong.Query do
   @spec with_logical_op(__MODULE__.t(), logical_op_name(), op(), op()) :: __MODULE__.t()
   def with_logical_op(%__MODULE__{ops: ops} = query, logical_op, op1, op2) when logical_op in @logical_op_names do
     %{query | ops: ops ++ [%{logical_op => [op1, op2]}]}
+  end
+
+  @doc """
+  Sets the query's selector to the specified key
+  """
+  @spec with_selector(__MODULE__.t(), selector(), String.t()) :: __MODULE__.t()
+  def with_selector(%__MODULE__{} = query, selector, key) when is_nil(selector) or selector in @selector_names do
+    if is_nil(selector) do
+      %{query | selector: nil}
+    else
+      %{query | selector: %{selector => key}}
+    end
   end
 end
