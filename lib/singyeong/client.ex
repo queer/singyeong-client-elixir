e25dbf239bf1c5b9e5941ad99191a958f7cf3b5b
amy null <null@amy.gg>
2021-05-23 01:58:50 -0400

    0.4.10 -- I'm literally the stupidest

--- lib/singyeong/client.ex
+++ lib/singyeong/client.ex
@@ -490,7 +490,8 @@ defmodule Singyeong.Client do
     Logger.debug "[PROXY] -> #{proxy_url}"
 
     res = HTTPoison.request! :post, proxy_url, proxy_body,
-        [{"Content-Type", "application/json"}, {"Authorization", auth}]
+        [{"Content-Type", "application/json"}, {"Authorization", auth}],
+        [timeout: @timeout, recv_timeout: @timeout]
 
     Jason.decode! res.body
   end
@@ -513,7 +514,8 @@ defmodule Singyeong.Client do
       |> Jason.encode!
 
     res = HTTPoison.request! :post, "#{protocol}://#{host}:#{port}/api/v1/query", body,
-        [{"Content-Type", "application/json"}, {"Authorization", auth}], [timeout: @timeout, recv_timeout: @timeout]
+        [{"Content-Type", "application/json"}, {"Authorization", auth}],
+        [timeout: @timeout, recv_timeout: @timeout]
 
     Jason.decode! res.body
   end
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Singyeong.MixProject do
   use Mix.Project
 
-  @version "0.4.9"
+  @version "0.4.10"
   @repo_url "https://github.com/queer/singyeong-client-elixir"
 
   def project do
